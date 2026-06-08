0efd16f2a6c8921abb79f47f96e8b74990f39de0
omaralispahic <oalispahic1@etf.unsa.ba>
2026-06-08 18:55:55 +0200

    Initial commit template

---
+++ include/easywifi.h
@@ -0,0 +1,6 @@
+#ifndef EASYWIFI_LIBRARY_H
+#define EASYWIFI_LIBRARY_H
+
+void hello();
+
+#endif // EASYWIFI_LIBRARY_H
---
+++ src/easywifi.cpp
@@ -0,0 +1,7 @@
+#include "../include/easywifi.h"
+
+#include <iostream>
+
+void hello() {
+    std::cout << "Hello, World!" << std::endl;
+}
