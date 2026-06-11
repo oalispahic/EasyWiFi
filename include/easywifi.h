0d5e55be29e199238f3e77bd898cdcd2c044ac17
omaralispahic <oalispahic1@etf.unsa.ba>
2026-06-11 18:01:34 +0200

    add: wifi connection class interface

--- include/easywifi.h
+++ include/easywifi.h
@@ -1,6 +1,52 @@
 #ifndef EASYWIFI_LIBRARY_H
 #define EASYWIFI_LIBRARY_H
 
-void hello();
+#include <cstdio>
+#include <vector>
+#include <string>
+
+enum class connectionStatus {
+    CONNECTED,
+    DISCONNECTED,
+    BAD_PASSWD,
+    SSID_NOT_FOUND,
+    OK,
+    ERR
+};
+
+
+struct networkInfo {
+    std::string ssid;
+    std::string pwd;
+    std::string signal_strength;
+};
+
+struct connectionState {
+    bool connected;
+    std::string ssid;
+    std::string interface; // wlan0
+};
+
+
+class networkConnector {
+public:
+    std::vector<networkInfo> networks;
+
+    networkConnector() = default;
+
+    connectionStatus scanNetworks();
+
+    connectionStatus connectNetwork(std::string ssid, std::string pwd);
+
+    connectionStatus disconnectNetwork(std::string ssid, std::string pwd);
+
+    connectionState getStatus();
+
+    connectionStatus radioOn();
+
+
+
+    static std::vector<std::string> execute(const std::string &cmd);
+};
 
 #endif // EASYWIFI_LIBRARY_H
--- src/easywifi.cpp
+++ src/easywifi.cpp
@@ -1,7 +1,25 @@
+//
+// Created by Omar Alispahic  on 11. 6. 2026..
+//
+
 #include "../include/easywifi.h"
 
-#include <iostream>
+std::vector<std::string> networkConnector::execute(const std::string &cmd) {
+    std::vector<std::string> lines;
 
-void hello() {
-    std::cout << "Hello, World!" << std::endl;
+    FILE *pipe = popen(cmd.c_str(),"r");
+
+    char buffer[256];
+    while (fgets(buffer,sizeof(buffer),pipe)) {
+        std::string line(buffer);
+
+        if (line.empty() && line.back() == '\n') line.pop_back();
+        lines.push_back(line);
+    }
+    return lines;
 }
+
+connectionStatus networkConnector::scanNetworks() {
+
+}
+
