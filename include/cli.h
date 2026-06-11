f07af4d6c001e67717670df0d66796717c4be5e1
omaralispahic <oalispahic1@etf.unsa.ba>
2026-06-11 18:01:06 +0200

    add: cli control header file

---
+++ include/cli.h
@@ -0,0 +1,14 @@
+//
+// Created by Omar Alispahic  on 11. 6. 2026..
+//
+
+#ifndef EASYWIFI_CLI_H
+#define EASYWIFI_CLI_H
+
+#include <string>
+#include <cstdio>
+#include <vector>
+
+
+
+#endif //EASYWIFI_CLI_H
